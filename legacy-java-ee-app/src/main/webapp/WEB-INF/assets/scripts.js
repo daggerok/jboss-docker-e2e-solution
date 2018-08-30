976fca8b2c93d73a9fb268fa03b9b2af3c222dfd
Maksim Kostromin <daggerok@gmail.com>
2018-08-30 03:22:44 +0300

    Update CI.

--- legacy-java-ee-app/src/main/webapp/WEB-INF/assets/scripts.js
+++ legacy-java-ee-app/src/main/webapp/WEB-INF/assets/scripts.js
@@ -1,6 +1,6 @@
 (function main() {
   'use strict';
   window.document.addEventListener('DOMContentLoaded', function onDOMContentLoaded(evt) {
-    console.log('hola!');
+    M.AutoInit();
   }, false);
 })();
